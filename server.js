23a56e4c212466b9a98cc5714d829fdcbe440a4e
mubassyir <amubassyir@gmail.com>
2020-11-23 19:48:15 +0700

    fix some bugs

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT ||8080;
 require('dotenv').config();
 
 let corsOption = {
-    origin : 'http://localhost:8080 ',
-    origin : 'http://localhost:3000 '   
+    origin : 'http://localhost:8080 '   
 }
 
 //Ejs Setting
